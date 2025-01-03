e83362658d7c22994fbc744d099d17822e086e16
BahubaliNalte <bahubalidnalte722006@gmail.com>
2025-01-03 16:10:28 +0530

    add jwt and cookie

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const userRouter = require('./routes/user.routes');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
+const cookieParser = require('cookie-parser');
 const connectToDB= require('./config/db')
 
 connectToDB();
 
 app.set('view engine', 'ejs');
+app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended:true}))
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {body, validationResult } = require('express-validator');
 const userModel = require('../models/user.model')
 const bcrypt = require('bcrypt');
+const jwt = require ('jsonwebtoken');
 
 
 router.get('/register', (req, res) => {
@@ -35,13 +36,66 @@ router.post('/register',
             password: hashPassword
         })
         
-        res.json(newUser)
-
-        
-         
+        res.json(newUser)      
 })
 
+router.get('/login', (req, res) =>{
+    res.render('login')
+})
 
+router.post('/login',
+    body('username').trim().isLength({ min:3 }),
+    body('password').trim().isLength({ min:5 }),
+    async (req, res) => {
+
+        const errors= validationResult(req);
+         
+        if (!errors.isEmpty()) {    
+            return res.status(400)
+        .json({
+            error: error.array(),
+            message: 'INVALID DATA'
+        })
+        }
+
+        const { username, password } = req.body;
+
+        const user = await userModel.findOne({
+            username: username
+        })
+
+        if(!user){
+            return res.status(400).json({
+                message: 'username or password is incorrect'
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password) 
+
+        if(!isMatch){
+            return res.status(400).json({
+                message: 'username or password is incorrect'
+            })
+        }
+
+        const token = jwt.sign({
+            userId: user._id,
+            email: user.email,
+            username: user.username  
+        },
+            process.env.JWT_SECRET,
+        )
+
+        res.cookie('token', token)
+
+        res.send('Logged In')
+
+    }
+)
+
+
+    
+        
 
 
 
